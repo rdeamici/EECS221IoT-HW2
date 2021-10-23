3284808eac6b85f1525e2d0ba8a6b8cf0da687c0
rdeamici <rtdeamicis@gmail.com>
2021-10-23 11:55:33 -0700

    added setup code for connecting lights to rpi

--- connect_esp8266.py
+++ connect_esp8266.py
@@ -1,1 +1,15 @@
+import time
+import board
 import sys
+import adafruit_tcs34725
+import RPi.GPIO as GPIO
+
+# Setup LED lights
+green = 5
+red = 6
+blue = 13
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(green, GPIO.OUT)
+GPIO.setup(red, GPIO.OUT)
+GPIO.setup(blue, GPIO.OUT)
