2768ca17278e5f91ab85310e18f1d17523af416d
rdeamici <rtdeamicis@gmail.com>
2021-10-24 11:22:51 -0700

    initial slow blink and check for connection done, untested

--- connect_esp8266.py
+++ connect_esp8266.py
@@ -24,6 +24,9 @@ RPiIP = '192.168.0.16'
 RPiPort = 4210
 RPiUDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
+#port to receive data from ESP8266
+ESPPort = 4210
+
 def setup_rpi():
     # Create sensor object, communicating via  I2C protocol
     i2c = board.I2C()
@@ -31,25 +34,39 @@ def setup_rpi():
 
     # start UDP server
     RPiUDPServerSocket.bind((RPiIP,RPiPort))
-    print("UDP server up and listening")
+    print("UDP server up and listening...")
 
-    return i2c,sensor
+    return i2c, sensor
 
 
 def check_connection():
-
+    data, address = RPiUDPServerSocket.recvfrom(ESPPort)
+    return data,address
 
 def flash_slowly():
     GPIO.output(RED,HIGH)
-    time.sleep(.5)
+    start = time.time()
+    data, address = check_connection()
+    end = time.time()
+    if end - start < 0.5:
+        time_to_sleep = 0.5 - (end-start)
+    else:
+        raise ValueError("took more than half ({}) a second to check connection".format(
+            end-start
+        ))
+    time.sleep(time_to_sleep)
+
 
 def wait_for_connection():
+    data = ''
+    while not data:
+        data, address = flash_slowly()    
     
+    return data, address
 
 def main()
     i2c,sensor = setup_rpi()
-    wait_for_connection()
-    socket.socket()
+    data, address = wait_for_connection()
 
 if __name__ == "__main__":
     main()
