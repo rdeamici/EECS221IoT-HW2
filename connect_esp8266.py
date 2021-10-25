d89ffd92562421799a7b823d09cbd32ca7c7b7c8
rdeamici <rtdeamicis@gmail.com>
2021-10-25 15:59:09 -0700

    rpi code still not done yet. not tested

--- connect_esp8266.py
+++ connect_esp8266.py
@@ -20,7 +20,7 @@ GPIO.setup(RED, GPIO.OUT)
 GPIO.setup(BLUE, GPIO.OUT)
 
 # setup UDP server on RPi
-RPiIP = '192.168.0.16'
+RPi_address = '192.168.0.16'
 RPiPort = 4210
 RPiUDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
@@ -33,7 +33,7 @@ def setup_rpi():
     sensor = adafruit_tcs34725.TCS34725(i2c)
 
     # start UDP server
-    RPiUDPServerSocket.bind((RPiIP,RPiPort))
+    RPiUDPServerSocket.bind((RPi_address,RPiPort))
     print("UDP server up and listening...")
 
     return i2c, sensor
@@ -41,32 +41,57 @@ def setup_rpi():
 
 def check_connection():
     data, address = RPiUDPServerSocket.recvfrom(ESPPort)
-    return data,address
+    return data, address
+
+def calculate_time_diff(start):
+    end = time.time()
+    time_to_sleep = 0.5 - (end - start)
+
+    if time_to_sleep < 0:
+        raise ValueError("took more than half a sec ({})  to check connection".format(end-start))
+
+    return time_to_sleep
+
 
 def flash_slowly():
+    '''
+    Represents a single on/off light blink cycle.
+    Blinks light half second on, half second off.
+    Checks for data sent via UDP during on period and
+    during off period, if no data was found previously.
+    '''
     GPIO.output(RED,HIGH)
     start = time.time()
-    data, address = check_connection()
-    end = time.time()
-    if end - start < 0.5:
-        time_to_sleep = 0.5 - (end-start)
-    else:
-        raise ValueError("took more than half ({}) a second to check connection".format(
-            end-start
-        ))
+    data, ESP_address = check_connection()
+    time_to_sleep = calculate_time_diff(start)
     time.sleep(time_to_sleep)
 
+    GPIO.output(RED,LOW)
+    start = time.time()
+
+    if not data:
+        data, ESP_address = check_connection()
+
+    time_to_sleep = calculate_time_diff(start)
+    time.sleep(time_to_sleep)
+
+    return data, ESP_address
 
 def wait_for_connection():
+    '''
+    flashes LED continuously while checking
+    for data sent via UDP connection
+    '''
     data = ''
     while not data:
         data, address = flash_slowly()    
     
     return data, address
 
-def main()
+def main():
     i2c,sensor = setup_rpi()
     data, address = wait_for_connection()
+    
 
 if __name__ == "__main__":
     main()
