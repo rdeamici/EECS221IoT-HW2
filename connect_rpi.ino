d425c5990a0234df817100f35b6522c8a9cfc078
rdeamici <rtdeamicis@gmail.com>
2021-10-25 17:33:30 -0700

    ESP code accurately connects to wifi, reads sensor, and sends reading to RPi. Untested if sending of data packet works

--- connect_rpi.ino
+++ connect_rpi.ino
@@ -1,18 +1,17 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
-// Trinh's wifi and password
-const char* ssid = "SETUP-564E-5"; 
-const char* password = "butler69"; 
+// Trinh's wifi info
+const char* SSID = "SETUP-564E-2.4"; 
+const char* PASSWORD = "butler15"; 
 
-// setup wifi
-WiFiUDP Udp; 
 unsigned int ESPUdpPort = 4210; // local port to listen on
-unsigned int RPiUdpPort = 4210;
+unsigned int RPiUdpPort = 4210; // port to send info to rpi
 
-char incomingPacket[255]; // buffer for incoming packets
- // a reply string to send back
+char incomingPacket[UDP_TX_PACKET_MAX_SIZE + 1]; // buffer for incoming packets
 char replyPacket[] = "Hi there! Got the message :-)";
+
+WiFiUDP Udp; 
 
 //set pin numbers
 const int BlueledPin = D0;
@@ -20,68 +19,75 @@ const int GreenledPin = D1;
 const int RedledPin = D2;
 const int ldrPin = A0;
 
-float checkCurrentLightLevel() {
+
+void setup() {
+  Serial.begin(115200);
+  // set pin modes
+  pinMode(BlueledPin, OUTPUT);
+  pinMode(GreenledPin, OUTPUT);
+  pinMode(RedledPin, OUTPUT);
+  pinMode(ldrPin, INPUT);
+
+  bool connected = false;
+
+  // blink light for 2 seconds
+  // while trying to connect to wifi
+  WiFi.begin(SSID, PASSWORD); 
+  Serial.print("Connecting to %s ");
+  Serial.println(SSID);
+  for(int i = 0; i < 10; i++) {
+    digitalWrite(RedledPin,HIGH);
+    delay(50);
+    digitalWrite(RedledPin,LOW);
+    delay(50);
+    if ( WiFi.status() != WL_CONNECTED) {
+      Serial.print('.');
+      // ensure loop continues until wifi connection established
+      if ( i == 9 )
+        i--;
+    }
+    else if (! connected) {
+      Serial.println(" connected!"); 
+      connected = true;
+    }
+  }
+  Serial.print("Ip address: ");
+  Serial.println(WiFi.localIP());
+
+  // ensure all lights are off
+  digitalWrite(RedledPin,LOW);
+  digitalWrite(GreenledPin,LOW);
+  digitalWrite(BlueledPin,LOW);
+
+  // setup server
+  Udp.begin(ESPUdpPort); 
+  Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), ESPUdpPort); 
+  digitalWrite(RedledPin, HIGH);
+}
+
+
+int checkCurrentLightLevel() {
     int ldrStatus = analogRead(ldrPin);  //read the state of the LDR value
     delay(250);
     return ldrStatus;
 }
 
 
-
-void setup() {
-    // set pin modes
-    pinMode(BlueledPin, OUTPUT);
-    pinMode(GreenledPin, OUTPUT);
-    pinMode(RedledPin, OUTPUT);
-    pinMode(ldrPin, INPUT);
-
-    // ensure all lights are off
-    Serial.begin(115200);
-    digitalWrite(RedledPin,LOW);
-    digitalWrite(GreenledPin,LOW);
-    digitalWrite(BlueledPin,LOW);
-
-    bool connected = false;
-    // blink light for 2 seconds
-    // while trying to connect to wifi
-    Serial.println();
-
-    Serial.printf("Connecting to %s ", ssid); 
-    WiFi.begin(ssid, password); 
-    for(int i = 0; i < 10; i++) {
-        digitalWrite(RedledPin,HIGH);
-        time.sleep(50);
-        digitalWrite(RedledPin,LOW);
-        time.sleep(50);
-        if ( WiFi.status() != WL_CONNECTED) {
-            Serial.printf(".");
-            
-            // ensure loop continues until wifi connection established
-            if ( i == 9 )
-                i--;
-        }
-        else if (! connected) {
-            Serial.println(" connected"); 
-            connected = true;
-        }
-    }
-
-    // setup server
-    Udp.begin(ESPUdpPort); 
-    Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), ESPUdpPort); 
-    digitalWrite(RedledPin, HIGH);
-}
-
 void loop() {
     // read sensor (0.25 seconds)
     int lightLvl = checkCurrentLightLevel();
+
     // convert int to char *
-    String msg2rpi = String(lightLvl)
+    String msg2rpi = String(lightLvl);
+
     // send data to the IP address of the RPi
+    Serial.print("light level: ");
+    Serial.println(lightLvl);
+    Serial.println("Sending to rpi");
     Udp.beginPacket('192.168.0.16', RPiUdpPort); 
     Udp.write(msg2rpi.c_str()); //convert msg to c-style char array
     Udp.endPacket();
-
+ 
     // attempt to receive incoming UDP packet from rpi
     int packetSize = Udp.parsePacket(); 
     if(packetSize) { 
@@ -93,7 +99,10 @@ void loop() {
             incomingPacket[len] = '\0';
         }
         Serial.println("UDP packet contents: ");
-        Serial.prinln(incomingPacket);
-
-    } 
+        Serial.println(incomingPacket);
+    }
+    else {
+        Serial.println("No UDP packet retrieved!");
+    }
+    delay(1000);
 }
